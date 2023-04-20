d01383f91bfdfca8481c34313acd0b3c281d85d4
Hezjod <129681681+Hezjod@users.noreply.github.com>
2023-04-20 18:47:22 +0200

    added iterator and fixed a mistake

--- static_array.py
+++ static_array.py
@@ -1,27 +1,33 @@
+from typing import Iterator
+
+
 class StaticArray:
     def __init__(self, item_type: type, size: int) -> None:
+        if not isinstance(item_type, type):
+            raise ValueError("item_type must be a class")
         self._item_type = item_type
         self._size = size
         self._list = [None] * size
-    
+
     def __getitem__(self, index: int):
         if index not in range(0, self._size):
             raise IndexError("StaticArray index out of range")
         return self._list[index]
 
-
     def __setitem__(self, index: int, item) -> None:
         if not isinstance(item, self._item_type):
-            raise TypeError(f"Invalid type ({type(item)}) for {self._item_type} StaticArray")
+            raise TypeError(
+                f"Invalid type ({type(item)}) for {self._item_type} StaticArray"
+            )
         if index not in range(0, self._size):
             raise IndexError("StaticArray index out of range")
         self._list[index] = item
-    
+
     def __repr__(self) -> str:
         return self._list.__repr__()
-    
+
     def __str__(self) -> str:
         return self._list.__str__()
 
-
-
+    def __iter__(self) -> Iterator:
+        return iter(self._list)
