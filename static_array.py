1177e8a89f913a46e95d67aaa6731f9875b3cf90
Hezjod <129681681+Hezjod@users.noreply.github.com>
2023-04-22 18:41:50 +0200

    typing fixes

--- static_array.py
+++ static_array.py
@@ -1,37 +1,37 @@
 from __future__ import annotations
-from typing import Iterator, TypeVar, Generic, Iterable, Type
+from typing import Iterator, TypeVar, Generic, Iterable
 
-T = TypeVar("T", bound=type)
+T = TypeVar("T")
 
 
 class StaticArray(Generic[T]):
-    def __init__(self, item_type: T, size: int) -> None:
+    def __init__(self, item_type: type[T], size: int) -> None:
         if size <= 0:
             raise ValueError("size has to be bigger than 0")
 
         if not isinstance(item_type, type):
             raise ValueError("item_type must be a class")
 
-        self._item_type: T = item_type
+        self._item_type: type[T] = item_type
         self._size: int = size
         self._list: list[T | None] = [None] * size
 
-    def __getitem__(self, key: int | slice) -> T | None | StaticArray[T]:
+    def __getitem__(self, key: int | slice) -> T | None | StaticArray[T | None]:
         if isinstance(key, slice):
             return StaticArray.from_iter(self._item_type, self._list[key])
 
         if isinstance(key, int):
             if key not in range(0, self._size):
-                raise IndexError(f"{type(self).__name__} index out of range")
+                raise IndexError(f"{__class__.__name__} index out of range")
             return self._list[key]
 
         raise TypeError(
-            f"StaticArray indices must be integers or slices, not {type(key).__name__}"
+            f"{__class__.__name__} indices must be integers or slices, not {type(key).__name__}"
         )
 
-    def __setitem__(self, key: int, item) -> None:
-        if isinstance(key, slice):
-            raise TypeError("you can't change StaticArray with a slice")
+    def __setitem__(self, key: int, item: T | None) -> None:
+        if not isinstance(key, int):
+            raise TypeError(f"StaticArray indices must be integers, not {type(key)}")
 
         if (not isinstance(item, self._item_type)) and (item is not None):
             raise TypeError(
@@ -39,7 +39,7 @@ class StaticArray(Generic[T]):
             )
 
         if key not in range(0, self._size):
-            raise IndexError(f"{type(self).__name__} index out of range")
+            raise IndexError(f"{__class__.__name__} index out of range")
 
         self._list[key] = item
 
@@ -52,10 +52,10 @@ class StaticArray(Generic[T]):
     def __iter__(self) -> Iterator[T | None]:
         return iter(self._list)
 
-    def index(self, item) -> int:
-        if not isinstance(item, self._item_type):
+    def index(self, item: T | None) -> int:
+        if (not isinstance(item, self._item_type)) and (item is not None):
             raise TypeError(
-                f"Invalid type ({type(item).__name__}) for {self._item_type.__name__} StaticArray"
+                f"Invalid type ({type(item).__name__}) for {self._item_type.__name__} {__class__.__name__}"
             )
         return self._list.index(item)
 
@@ -63,16 +63,18 @@ class StaticArray(Generic[T]):
         self._list = [None] * self._size
 
     @staticmethod
-    def from_iter(item_type: T, arr: Iterable[T | None]) -> StaticArray[T]:
-        items = list(iter(arr))
+    def from_iter(item_type: type[T], arr: Iterable[T | None]) -> StaticArray[T | None]:
+        items: list[T | None] = list(iter(arr))
         if len(items) == 0:
-            raise ValueError("can't create StaticArray from empty iterable")
+            raise ValueError(
+                f"can't create {__class__.__name__} from an empty iterable"
+            )
 
-        new_static_array = StaticArray(item_type, len(items))
+        new_static_array: StaticArray[T | None] = StaticArray(item_type, len(items))
         for index, item in enumerate(items):
             if (not isinstance(item, item_type)) and (item is not None):
                 raise TypeError(
-                    f"Invalid type ({type(item)}) for {item_type} StaticArray"
+                    f"Invalid type ({type(item)}) for {item_type} {__class__.__name__}"
                 )
             new_static_array[index] = item
 
