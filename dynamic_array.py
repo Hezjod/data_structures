243f8f73613fe85c242e2173a590eab4272ba3dc
Hezjod <129681681+Hezjod@users.noreply.github.com>
2023-04-22 21:33:00 +0200

    imporved type hints, added __iter__()

--- dynamic_array.py
+++ dynamic_array.py
@@ -1,78 +1,88 @@
+from __future__ import annotations
+from typing import Generic, TypeVar, Iterator
+
 from static_array import StaticArray
 from data_loss_warning import DataLossWarning
 
+T = TypeVar("T")
 
-class DynamicArray:
-    def __init__(self, item_type: type, *, initial_capacity: int = 4) -> None:
+
+class DynamicArray(Generic[T]):
+    def __init__(self, item_type: type[T], *, initial_capacity: int = 4) -> None:
         if not isinstance(item_type, type):
             raise ValueError("item_type must be a class")
 
         if initial_capacity < 0:
-            raise ValueError("Capacity can't be negative")
+            raise ValueError("capacity can't be negative")
 
-        self._size = 0
-        self._item_type = item_type
-        self._capacity = initial_capacity
-        self._array = StaticArray(self._item_type, self._capacity)
+        self._length: int = 0
+        self._item_type: type[T] = item_type
+        self._capacity: int = initial_capacity
+        self._array: StaticArray[T] = StaticArray(self._item_type, self._capacity)
 
     def _resize(self, new_capacity: int, *, ignore_warnings=False) -> None:
         if new_capacity < 0:
-            raise ValueError("Capacity can't be negative")
+            raise ValueError("capacity can't be negative")
 
         if new_capacity == self._capacity:
-            return None
+            return
 
-        if (not ignore_warnings) and new_capacity < self._size:
-            raise DataLossWarning("You might loss some data due to this operation")
+        if (not ignore_warnings) and new_capacity < self._length:
+            raise DataLossWarning("you might loss some data due to this operation")
 
-        new_array = StaticArray(self._item_type, new_capacity)
+        new_array: StaticArray[T] = StaticArray(self._item_type, new_capacity)
         for index in range(min(new_capacity, self._capacity)):
             new_array[index] = self._array[index]
 
         self._array = new_array
         self._capacity = new_capacity
-        self._size = min(self._size, new_capacity)
+        self._length = min(self._length, new_capacity)
 
-    def append(self, item) -> None:
-        if (not isinstance(item, self._item_type)) and (item is not None):
+    def append(self, item: T) -> None:
+        if not isinstance(item, self._item_type):
             raise TypeError(
-                f"Invalid type ({type(item)}) for {self._item_type} StaticArray"
+                f"invalid type ({type(item)}) for {self._item_type} StaticArray"
             )
 
-        if self._size == self._capacity:
+        if self._length == self._capacity:
             self._resize(self._capacity * 2)
 
-        self._array[self._size] = item
-        self._size += 1
+        self._array[self._length] = item
+        self._length += 1
 
-    def pop(self, index: int = -1):
+    def pop(self, index: int = -1) -> T:
         if not isinstance(index, int):
             raise TypeError(f"index must be of type int, not {type(index)}")
 
         if index < 0:
-            index = self._size + index
+            index = self._length + index
 
-        if index >= self._size:
+        if index >= self._length:
             raise IndexError("DynamicArray index out of range")
 
-        for i in range(index, self._size - 1):
+        removed = self._array[index]
+
+        for i in range(index, self._length - 1):
             self._array[i] = self._array[i + 1]
 
-        self._size -= 1
+        self._length -= 1
 
-        self._array[self._size] = None
+        self._array[self._length] = None
 
-    def __getitem__(self, key: int | slice):
+        return removed  # type: ignore # returning None is impossible here
+
+    def __getitem__(self, key: int | slice) -> T | DynamicArray[T]:
         if isinstance(key, slice):
+            raise NotImplementedError
             return self._list[key]
 
         if isinstance(key, int):
-            if key not in range(0, self._size):
-                raise IndexError("DynamicArray index out of range")
-            return self._list[key]
+            if key not in range(0, self._length):
+                raise IndexError(f"{__class__.__name__} index out of range")
+            return self._array[key]  # type: ignore # returning None is impossible here
 
         raise TypeError(
-            f"DynamicArray indices must be integers or slices, not {type(key)}"
+            f"{__class__.__name__} indices must be integers or slices, not {type(key)}"
         )
 
     def __setitem__(self, key: int, item) -> None:
@@ -81,26 +91,29 @@ class DynamicArray:
 
         if (not isinstance(item, self._item_type)) and (item is not None):
             raise TypeError(
-                f"Invalid type ({type(item)}) for {self._item_type} StaticArray"
+                f"invalid type ({type(item)}) for {self._item_type} StaticArray"
             )
 
-        if key not in range(0, self._size):
-            raise IndexError("StaticArray index out of range")
+        if key not in range(0, self._length):
+            raise IndexError(f"{__class__.__name__} index out of range")
 
-        self._list[key] = item
+        self._array[key] = item
 
     def __str__(self) -> str:
-        return self._array[: self._size].__str__()
+        return self._array[: self._length].__str__()
 
     def __repr__(self) -> str:
-        return self._array[: self._size].__repr__()
+        return self._array[: self._length].__repr__()
 
     def __len__(self) -> int:
-        return self._size
+        return self._length
 
-    def index(self, item) -> int:
+    def index(self, item: T) -> int:
         if not isinstance(item, self._item_type):
             raise TypeError(
                 f"Invalid type ({type(item)}) for {self._item_type} StaticArray"
             )
         return self._array.index(item)
+
+    def __iter__(self) -> Iterator[T]:
+        return iter(self._array[: self._length])  # type: ignore # returning None is impossible here
